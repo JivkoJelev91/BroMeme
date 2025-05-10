c4f9a7509fceb0091963ff8832a9336d91deaef7
zhivko.zhelev <zhivko.zhelev@egt-digital.com>
2025-05-10 22:49:28 +0300

    viteConfig fix

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,12 @@ import path from 'path'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+   base: './', // Use relative pat
+   build: {
+    outDir: 'dist',
+    // Ensure source maps are generated
+    sourcemap: true
+  },
   resolve: {
     alias: {
       src: path.resolve(__dirname, './src'),
